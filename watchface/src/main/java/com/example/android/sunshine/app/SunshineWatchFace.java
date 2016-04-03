59dc1fba327d67300730f0e4aa2424510174a335
pmatushkin <pmatushkin@gmail.com>
2016-04-03 12:36:55 -0400

    feat: Change watch face peek mode

--- watchface/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
+++ watchface/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
@@ -223,7 +223,7 @@ public class SunshineWatchFace extends CanvasWatchFaceService {
             super.onCreate(holder);
 
             setWatchFaceStyle(new WatchFaceStyle.Builder(SunshineWatchFace.this)
-                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
                     .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
                     .setShowSystemUiTime(false)
                     .build());
